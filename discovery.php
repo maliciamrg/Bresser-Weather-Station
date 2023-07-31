fd1f13cafe404f2082ef0aec215c18c07de7bf4a
MaliciaMrg <maliciamrg@users.noreply.github.com>
2023-07-31 02:47:30 +0200

    Update discovery.php

--- discovery.php
+++ discovery.php
@@ -1,16 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Publication MQTT Discovery pour la station météo</title>
+</head>
+<body>
+<p>
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once ('vendor/autoload.php');
+
+use PhpMqtt\Client\Exceptions\ConnectingToBrokerFailedException;
+use PhpMqtt\Client\Exceptions\DataTransferException;
+use PhpMqtt\Client\Exceptions\UnexpectedAcknowledgementException;
+use PhpMqtt\Client\ConnectionSettings;
+use PhpMqtt\Client\MqttClient;
 
 // Configuration MQTT
-$mqttBroker = "tcp://your_mqtt_broker_ip:1883";
-$mqttClientID = "mqtt_discovery_script";
-$mqttUsername = "your_mqtt_username";
-$mqttPassword = "your_mqtt_password";
+const MQTT_HOST = '192.212.30.105';
+const MQTT_PORT = 1883;
+const MQTT_CLIENT_ID = 'mqtt_discovery_script';
+const MQTT_USER = 'mqttuser';
+const MQTT_PASSWORD = 'mqttuser';
 
 // Connexion au broker MQTT
-$mqtt = new Mosquitto\Client($mqttClientID);
-$mqtt->setCredentials($mqttUsername, $mqttPassword);
-$mqtt->connect($mqttBroker);
-$mqtt->loop();
+// Send it to MQTT
+$mqtt = new MqttClient(MQTT_HOST, MQTT_PORT, MQTT_CLIENT_ID);
+$settings = (new ConnectionSettings)
+    ->setUsername(MQTT_USER)
+    ->setPassword(MQTT_PASSWORD);
+$mqtt->connect($settings, true);
 
 // Fonction pour publier le JSON de découverte pour un capteur
 function publishDiscoveryJSON($sensorName, $sensorType, $deviceClass, $unitOfMeasurement, $stateTopic, $icon) {
@@ -18,9 +39,8 @@ function publishDiscoveryJSON($sensorName, $sensorType, $deviceClass, $unitOfMea
     $deviceIdentifier = "pws_5_in_1_001"; // Remplacez par l'identifiant unique de votre station météo
 
     $json = array(
-        "name" => "Station Meteo - " . $sensorName,
+        "name" => "Station Meteo " . $sensorName,
         "uniq_id" => $deviceIdentifier . "_" . strtolower($sensorName),
-        "device_class" => $deviceClass,
         "state_topic" => $stateTopic,
         "unit_of_measurement" => $unitOfMeasurement,
         "icon" => $icon,
@@ -33,27 +53,41 @@ function publishDiscoveryJSON($sensorName, $sensorType, $deviceClass, $unitOfMea
         )
     );
 
+    // Ajouter "device_class" au JSON uniquement si $deviceClass n'est pas vide
+    if (!empty($deviceClass)) {
+        $json["device_class"] = $deviceClass;
+    }
+
     $topic = "homeassistant/sensor/" . $deviceIdentifier . "/" . strtolower($sensorName) . "/config";
     $payload = json_encode($json);
     
     global $mqtt;
     $mqtt->publish($topic, $payload, 1, true);
-    $mqtt->loop();
+	sleep(2);
+	// Affichage du JSON publié
+    echo "JSON de découverte pour le capteur $sensorName publié :<br>";
+    echo "<pre>" . htmlentities($payload) . "</pre>";
 }
 
 // Publier le JSON de découverte pour chaque capteur
-publishDiscoveryJSON("Baromin", "sensor", "pressure", "hPa", "pws/sensor/baromin", "mdi:gauge");
-publishDiscoveryJSON("Temp", "sensor", "temperature", "°C", "pws/sensor/temp", "mdi:thermometer");
-publishDiscoveryJSON("Dewpt", "sensor", "temperature", "°C", "pws/sensor/dewpt", "mdi:thermometer");
-publishDiscoveryJSON("Humidity", "sensor", "humidity", "%", "pws/sensor/humidity", "mdi:water-percent");
-publishDiscoveryJSON("Windspeedkph", "sensor", "speed", "km/h", "pws/sensor/windspeedkph", "mdi:weather-windy");
-publishDiscoveryJSON("Windgustkph", "sensor", "speed", "km/h", "pws/sensor/windgustkph", "mdi:weather-windy");
-publishDiscoveryJSON("Winddirection", "sensor", "direction", "°", "pws/sensor/winddirection", "mdi:compass");
-publishDiscoveryJSON("Rainmm", "sensor", "rain", "mm", "pws/sensor/rainmm", "mdi:weather-rainy");
-publishDiscoveryJSON("Dailyrainmm", "sensor", "rain", "mm", "pws/sensor/dailyrainmm", "mdi:weather-rainy");
-publishDiscoveryJSON("Indoortemp", "sensor", "temperature", "°C", "pws/sensor/indoortemp", "mdi:thermometer");
-publishDiscoveryJSON("Indoorhumidity", "sensor", "humidity", "%", "pws/sensor/indoorhumidity", "mdi:water-percent");
+publishDiscoveryJSON("Baromin", "sensor", "atmospheric_pressure", "hPa", "pws_5_in_1_001/sensors/baromin", "mdi:gauge");
+publishDiscoveryJSON("OutdoorTemp", "sensor", "temperature", "°C", "pws_5_in_1_001/sensors/temp", "mdi:thermometer");
+publishDiscoveryJSON("Dewpt", "sensor", "temperature", "°C", "pws_5_in_1_001/sensors/dewpt", "mdi:thermometer");
+publishDiscoveryJSON("OutdoorHumidity", "sensor", "humidity", "%", "pws_5_in_1_001/sensors/humidity", "mdi:water-percent");
+publishDiscoveryJSON("WindSpeedKph", "sensor", "wind_speed", "km/h", "pws_5_in_1_001/sensors/windspeedkph", "mdi:weather-windy");
+publishDiscoveryJSON("WindGustKph", "sensor", "wind_speed", "km/h", "pws_5_in_1_001/sensors/windgustkph", "mdi:weather-windy");
+publishDiscoveryJSON("WindDirection", "sensor", "", "°", "pws_5_in_1_001/sensors/winddirection", "mdi:compass");
+publishDiscoveryJSON("RainMm", "sensor", "precipitation", "mm", "pws_5_in_1_001/sensors/rainmm", "mdi:weather-rainy");
+publishDiscoveryJSON("DailyRainMm", "sensor", "precipitation", "mm", "pws_5_in_1_001/sensors/dailyrainmm", "mdi:weather-rainy");
+publishDiscoveryJSON("IndoorTemp", "sensor", "temperature", "°C", "pws_5_in_1_001/sensors/indoortemp", "mdi:thermometer");
+publishDiscoveryJSON("IndoorHumidity", "sensor", "humidity", "%", "pws_5_in_1_001/sensors/indoorhumidity", "mdi:water-percent");
 
 // Déconnexion du broker MQTT
 $mqtt->disconnect();
 unset($mqtt);
+
+?>
+success
+</p>
+</body>
+</html>
